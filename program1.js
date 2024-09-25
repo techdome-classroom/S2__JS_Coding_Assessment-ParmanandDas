8d4d4c99ec6fb1123e49ea3543035a83fb8de6c9
ParmanandDas <144559608+ParmanandDas@users.noreply.github.com>
2024-09-25 15:39:15 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -8,4 +8,31 @@ var isValid = function(s) {
 
 module.exports = { isValid };
 
+function isValid(s) {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
 
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            let last = stack.pop();
+            if (map[last] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+// Example usage:
+console.log(isValid("()"));        // true
+console.log(isValid("()[]{}"));    // true
+console.log(isValid("(]"));        // false
