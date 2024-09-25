df7fa4259cd7b63304c079dd2d93cec96c30e648
ParmanandDas <144559608+ParmanandDas@users.noreply.github.com>
2024-09-25 15:40:25 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -37,7 +37,7 @@ function romanToInt(s) {
     return total;
 }
 
-// Example usage:
+
 console.log(romanToInt("III"));      // Output: 3
 console.log(romanToInt("LVIII"));    // Output: 58
 console.log(romanToInt("MCMXCIV"));  // Output: 1994
